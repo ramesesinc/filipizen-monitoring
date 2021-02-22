85f94dd42bca09dc2765732ba17676731e71b432
jzamss@gmail.com <jzamss@gmail.com>
2021-02-22 10:44:29 +0800

    add backend support

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import styles from "./Content.css";
-import { Service} from "rsi-react-web-components";
+import { Service } from "rsi-react-web-components";
 
 const partnerSvc = Service.lookup("CloudPartnerService", "partner");
 const txnSvc = Service.lookup("CloudPaymentMonitoringService", "epayment");
@@ -9,6 +9,7 @@ const Content = (props) => {
   const [lgus, setLgus] = useState([]);
   const [payPartners, setPayPartners] = useState([]);
   const [partners, setPartners] = useState([]);
+  const [period, setPeriod] = useState("");
   const [loading, setLoading] = useState(true);
 
   const lgusRef = useRef([]);
@@ -16,10 +17,10 @@ const Content = (props) => {
   useEffect(() => {
     partnerSvc.invoke("getActivePartners", null, (err, lguList) => {
       if (!err) {
-        const lgus = lguList.map(lgu => ({id: lgu.id, name: lgu.name}))
+        const lgus = lguList.map((lgu) => ({ id: lgu.id, name: lgu.name }));
         setLgus(lgus);
         lgusRef.current = lgus;
-      } 
+      }
     });
   }, []);
 
@@ -28,12 +29,13 @@ const Content = (props) => {
 
     txnSvc.invoke("getInitialInfo", {}, (err, info) => {
       if (!err) {
-        info.partners.forEach(partner => {
-          const lgu = lgus.find(lgu => lgu.id === partner.id);
+        info.partners.forEach((partner) => {
+          const lgu = lgus.find((lgu) => lgu.id === partner.id);
           partner.name = lgu.name;
-        })
+        });
         setPartners(info.partners);
         setPayPartners(info.paypartners);
+        setPeriod(info.period);
       } else {
         console.log("Error loading partners ", err);
       }
@@ -45,48 +47,58 @@ const Content = (props) => {
     const intervalId = setInterval(() => {
       txnSvc.invoke("getTxnCounts", {}, (err, partners) => {
         if (!err) {
-          partners.forEach(partner => {
-            const lgu = lgusRef.current.find(lgu => lgu.id === partner.id);
+          partners.forEach((partner) => {
+            const lgu = lgusRef.current.find((lgu) => lgu.id === partner.id);
             partner.name = lgu.name;
-          })
+          });
           setPartners(partners);
         }
       });
     }, 5000);
-    return () => {clearInterval(intervalId)};
-  }, [])
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
 
   if (loading) {
-    return <div>Loading</div>
+    return <div>Loading</div>;
   }
 
   return (
-    <div className={styles.Content}>
-      <center>
-        <h1>ePayment Monitoring</h1>
-      </center>
-      <div className={styles.Content__header}>
-        <p className={styles.Content__lgu}>LGU</p>
-        {payPartners.map((payPartner) => (
-          <p key={payPartner.name} className={styles.Content__partners}>{payPartner.name}</p>
-        ))}
-      </div>
-      {partners.map((partner) => {
-        const paymentComponents = payPartners.map((payPartner) => {
-          return (
-            <p key={partner.id + ":" + payPartner.name} className={styles.Content__partners}>
-              {" "}
-              {partner.paypartner[payPartner.name.toLowerCase()]}{" "}
+    <div>
+      <div className={styles.Content}>
+        <center>
+          <h1>ePayment Monitoring</h1>
+          <h3>{period}</h3>
+        </center>
+        <div className={styles.Content__header}>
+          <p className={styles.Content__lgu}>LGU</p>
+          {payPartners.map((payPartner) => (
+            <p key={payPartner.name} className={styles.Content__partners}>
+              {payPartner.name}
             </p>
+          ))}
+        </div>
+        {partners.map((partner) => {
+          const paymentComponents = payPartners.map((payPartner) => {
+            const count = partner.paypartner[payPartner.name.toLowerCase()];
+            return (
+              <p
+                key={partner.id + ":" + payPartner.name}
+                className={styles.Content__partners}
+              >
+                {count > 0 && count}
+              </p>
+            );
+          });
+          return (
+            <div key={partner.name} className={styles.Content__subContainer}>
+              <p className={styles.Content__lgu}> {partner.name} </p>
+              {paymentComponents}
+            </div>
           );
-        });
-        return (
-          <div key={partner.name} className={styles.Content__subContainer}>
-            <p className={styles.Content__lgu}> {partner.name} </p>
-            {paymentComponents}
-          </div>
-        );
-      })}
+        })}
+      </div>
     </div>
   );
 };
