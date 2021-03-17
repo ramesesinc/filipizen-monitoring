34aee08c7559729861f9b53df957e15dec1bce7e
jdcanonoy21 <jdcanonoy21@gmail.com>
2021-03-17 01:44:55 -0700

    update

--- src/Monitor.js
+++ src/Monitor.js
@@ -1,11 +1,12 @@
 import React from "react";
 import MasterTemplate from "./templates/MasterTemplate";
 import Content from "./components/Content";
+import Graph from "./components/Graph";
 
 function Monitor() {
   return (
     <MasterTemplate>
-      <Content />
+      <Graph />
     </MasterTemplate>
   );
 }
